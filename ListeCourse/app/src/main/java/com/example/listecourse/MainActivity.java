177d9045a0540e83cf870f9d002f3f0ac10bcaa7
Sinsay63 <yanis.houdier@gmail.com>
2022-04-18 00:03:40 +0200

    Ne pas prendre en compte le dernier push "push final"

--- ListeCourse/app/src/main/java/com/example/listecourse/MainActivity.java
+++ ListeCourse/app/src/main/java/com/example/listecourse/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        deleteDatabase("listeCourse.db");
+        //deleteDatabase("listeCourse.db");
 
         btnProduit = findViewById(R.id.btnProduit);
         btnListe = findViewById(R.id.btnListe);
